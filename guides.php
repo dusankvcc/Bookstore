b713b4fc38587d399881819b430bd3a81578056a
Dušan <dusan.kovacic@student.um.si>
2023-11-29 14:26:19 +0100

    1stCommit

---
+++ guides.php
@@ -0,0 +1,42 @@
+<!-- 
+FOLDER HIERARCHY    
+
+online-bookstore/
+│
+├── index.html                # Main entry point of your website
+│
+├── css/                      # Directory for CSS files
+│   ├── style.css             # Main stylesheet
+│   └── ...                   # Other CSS files
+│
+├── js/                       # Directory for JavaScript files
+│   ├── main.js               # Main JavaScript file
+│   └── ...                   # Other JS files or libraries
+│
+├── images/                   # Directory for all images
+│   ├── logo.png              # Example image
+│   └── ...                   # Other images
+│
+├── fonts/                    # Directory for custom fonts
+│   └── ...                   # Font files
+│
+├── lib/                      # Directory for third-party libraries (if not using a package manager)
+│   └── ...                   # Library files like jQuery, Bootstrap, etc.
+│
+├── pages/                    # HTML files for additional pages
+│   ├── about.html            # About page
+│   ├── contact.html          # Contact page
+│   └── ...                   # Other HTML pages
+│
+├── partials/                 # Directory for HTML partials (e.g., header, footer)
+│   ├── header.html           # Header partial
+│   ├── footer.html           # Footer partial
+│   └── ...                   # Other partials
+│
+├── php/                      # PHP files (if using PHP)
+│   ├── database.php          # Database connection
+│   └── ...                   # Other PHP scripts
+│
+├── .gitignore                # Specifies intentionally untracked files to ignore
+├── README.md                 # Project description and instructions
+└── package.json              # Project metadata and dependencies (if using Node.js) -->
